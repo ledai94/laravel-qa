3cfded53ee1689779ca8c1a3224e6a9b3fa55642
le dai <dailv@vnext.com.vn>
2021-10-05 13:58:15 +0700

    2.3 generate question schema 2

--- app/User.php
+++ app/User.php
@@ -26,4 +26,12 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+    public function question(){
+        return $this->hasMany(Question::class);
+    }
+    public function setTitleAttribute($value){
+        $this->attribute['title']=$value;
+        $this->attribute['slug'] =str_slug($value);
+    }
+
 }
