48d2bb22b0990213b8b9d3da405ac6d3af624873
le dai <dailv@vnext.com.vn>
2021-10-07 15:57:12 +0700

    2.17 delete question

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -16,7 +16,7 @@
 
                 <div class="card-body">
                     @include('layouts._messages')
-                    <hr>
+                    
                     @foreach($questions as $question)
                     <div class='media'>
                         <div class="d-flex flex-column counters">
@@ -39,6 +39,11 @@
                                 </h3>
                                 <div class="ml-auto">
                                     <a href="{{route('questions.edit',$question->id)}}" class="btn btn-sm btn-outline-info">Edit</a>
+                                    <form class='form-delete' action="{{route('questions.destroy',$question->id)}}" method="post">
+                                        @method("DELETE")
+                                        @csrf
+                                        <button  type='submit' class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you sure?')">Delete</button>
+                                    </form>
                                 </div>
                             </div>
 
