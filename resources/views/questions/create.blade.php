1538b2fb6075581fd7e52bb64835585a503026f0
le dai <dailv@vnext.com.vn>
2021-10-07 11:28:01 +0700

    fix form

--- resources/views/questions/create.blade.php
+++ resources/views/questions/create.blade.php
@@ -13,33 +13,33 @@
                         </div>
                     </div>
             </div>
-                <div class="card-body">
-                    <form action="{{route('questions.store')}}" method="post">
+            <div class="card-body">
+                   <form action="{{ route('questions.store') }}" method="post">
                         @csrf
                         <div class="form-group">
-                            <label for="question-title">Question title</label>
-                            <input type="text" name="title" id="question-title" class='form-control {{$errors->has("title")? "is-invalid":""}}'>
-                            @if($errors->has('title'))
-                                <div class='invalid-feedback'>
-                                    <strong>{{$errors->first('title')}}</strong>
-                                </div>
-                            @endif
-                        </div>
-                        <div class='form-group'>
-                            <label for="question-body">Explain you question</label>
-                            <textarea name="body" id="question-body"  rows="10" class='form-control {{$errors->has("body")?"is-invalid":""}}'></textarea>
-                            @if($errors->has('body'))
-                                <div class='invalid-feedback'>
-                                    <strong>{{$errors->first('body')}}</strong>
+                            <label for="question-title">Question Title</label>
+                            <input type="text" name="title" id="question-title" class="form-control {{ $errors->has('title') ? 'is-invalid' : '' }}">
+
+                            @if ($errors->has('title'))
+                                <div class="invalid-feedback">
+                                    <strong>{{ $errors->first('title') }}</strong>
                                 </div>
                             @endif
                         </div>
                         <div class="form-group">
-                            <div class="btn btn-outline-primary btn-lg" type="submit">
-                                Ask this question
-                            </div>
+                            <label for="question-body">Explain you question</label>
+                            <textarea name="body" id="question-body" rows="10" class="form-control {{ $errors->has('body') ? 'is-invalid' : '' }}"></textarea>
+
+                            @if ($errors->has('body'))
+                                <div class="invalid-feedback">
+                                    <strong>{{ $errors->first('body') }}</strong>
+                                </div>
+                            @endif
                         </div>
-                    </form>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-outline-primary btn-lg">Ask this question</button>
+                        </div>
+                   </form>
                 </div>
             </div>
         </div>
