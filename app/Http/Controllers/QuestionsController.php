635be1cb7f3032a619d3db87a88f11a7df7d5deb
le dai <dailv@vnext.com.vn>
2021-10-07 14:42:49 +0700

    updateing the question 1

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -14,15 +14,10 @@ class QuestionsController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function index()
-    {
-        // \DB::enableQueryLog();
-        // $questions = Question::latest()->paginate(10);
-
+    {        
         $questions = Question::with('user')->latest()->paginate(10);
-        return view('questions.index',compact('questions'))->render();
-        // dd(\DB::getQueryLog());
 
-        //
+        return view('questions.index', compact('questions'));        
     }
 
     /**
@@ -33,7 +28,8 @@ class QuestionsController extends Controller
     public function create()
     {
         $question = new Question();
-        return view('questions.create',compact('question'));
+
+        return view('questions.create', compact('question'));
     }
 
     /**
@@ -44,19 +40,18 @@ class QuestionsController extends Controller
      */
     public function store(AskQuestionRequest $request)
     {
-        $request->user()->questions()->create($request->only('title','body'));
-        return redirect()->route('questions.index')->with('success','Your question has been submitted');
+        $request->user()->questions()->create($request->only('title', 'body'));
 
-        //
+        return redirect()->route('questions.index')->with('success', "Your question has been submitted");
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Questions  $questions
+     * @param  \App\Question  $question
      * @return \Illuminate\Http\Response
      */
-    public function show(Questions $questions)
+    public function show(Question $question)
     {
         //
     }
@@ -64,10 +59,10 @@ class QuestionsController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Questions  $questions
+     * @param  \App\Question  $question
      * @return \Illuminate\Http\Response
      */
-    public function edit(Questions $questions)
+    public function edit(Question $question)
     {
         //
     }
@@ -76,10 +71,10 @@ class QuestionsController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Questions  $questions
+     * @param  \App\Question  $question
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Questions $questions)
+    public function update(Request $request, Question $question)
     {
         //
     }
@@ -87,10 +82,10 @@ class QuestionsController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Questions  $questions
+     * @param  \App\Question  $question
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Questions $questions)
+    public function destroy(Question $question)
     {
         //
     }
