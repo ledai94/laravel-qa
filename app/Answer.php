2521051359679c6f0372f9635fba0ad2c1780ed8
le dai <dailv@vnext.com.vn>
2021-10-12 10:48:38 +0700

    3.12 delete answer

--- app/Answer.php
+++ app/Answer.php
@@ -26,16 +26,21 @@ class Answer extends Model
     public static function boot()
     {
         parent::boot();
-
         static::created(function ($answer) {
             $answer->question->increment('answers_count');
-            $answer->question->save();            
-        });        
+        });   
+        static::deleted(function($answer){
+            $answer->question->decrement('answers_count');
+        });
+
     }
     public function getCreatedDateAttribute(){
         return $this->created_at->diffForHumans();
     }
     public function FunctionName(Type $var = null){
 
+    }
+    public function getStatusAttribute(){
+        return $this->id === $this->question->best_answer_id ? 'vote-accepted':'';
     }
 }
