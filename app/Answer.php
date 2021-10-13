172c9e8f91f9d4454e0c25892c7212127c76bc07
le dai <dailv@vnext.com.vn>
2021-10-13 11:35:33 +0700

    4.3 vote answer

--- app/Answer.php
+++ app/Answer.php
@@ -26,28 +26,48 @@ class Answer extends Model
     public static function boot()
     {
         parent::boot();
-        static::created(function ($answer) {
-            $answer->question->increment('answers_count');
-        });   
-        static::deleted(function($answer){
-            $answer->question->decrement('answers_count');
-        });
 
+        static::created(function ($answer) {
+            $answer->question->increment('answers_count');                     
+        });        
+
+        static::deleted(function ($answer) {            
+            $answer->question->decrement('answers_count');            
+        });
     }
-    public function getCreatedDateAttribute(){
+
+    public function getCreatedDateAttribute()
+    {
         return $this->created_at->diffForHumans();
     }
 
-    public function getStatusAttribute(){
-        return $this->isBest() ? 'vote-accepted' : "";
+    public function getStatusAttribute()
+    {
+        return $this->isBest() ? 'vote-accepted' : '';
     }
-    public function getIsbestAttribute(){
+
+    public function getIsBestAttribute()
+    {
         return $this->isBest();
     }
-    public function isBest(){
+
+    public function isBest()
+    {
         return $this->id === $this->question->best_answer_id;
     }
-    public function votes(){
-        return $this->morphToMany(User::class,'voteable');
+
+    public function votes()
+    {
+        return $this->morphToMany(User::class, 'votable');
+    }
+
+    public function upVotes()
+    {
+        return $this->votes()->wherePivot('vote', 1);
+    }
+
+    public function downVotes()
+    {
+        return $this->votes()->wherePivot('vote', -1);
     }
 }
--- app/User.php
+++ app/User.php
@@ -26,24 +26,29 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
-    public function questions(){
+
+    public function questions()
+    {
         return $this->hasMany(Question::class);
-    }
-    // public function setTitleAttribute($value){
-    //     $this->attribute['title']=$value;
-    //     $this->attribute['slug'] =str_slug($value);
-    // }
-    public function getUrlAttribute(){
-        // return route("questions.show",$this->id);
+    }  
+    
+    public function getUrlAttribute()
+    {
+        // return route("questions.show", $this->id);
         return '#';
     }
-    public function answers(){
+
+    public function answers()
+    {
         return $this->hasMany(Answer::class);
     }
-    public function getAvatarAttribute(){
-        $email=$this->email;
-        $size=32;
-        return "https://www.gravatar.com/avatar/" . md5(strtolower(trim($email))) . "?s=" . $size;
+
+    public function getAvatarAttribute()
+    {
+        $email = $this->email;        
+        $size = 32;
+
+        return "https://www.gravatar.com/avatar/" . md5( strtolower( trim( $email ) ) ) . "?s=" . $size;
     }
 
     public function favorites()
@@ -51,5 +56,49 @@ class User extends Authenticatable
         return $this->belongsToMany(Question::class, 'favorites')->withTimestamps(); //, 'author_id', 'question_id');
     }
 
+    public function voteQuestions()
+    {
+        return $this->morphedByMany(Question::class, 'votable');
+    }
 
+    public function voteAnswers()
+    {
+        return $this->morphedByMany(Answer::class, 'votable');
+    }
+
+    public function voteQuestion(Question $question, $vote)
+    {
+        $voteQuestions = $this->voteQuestions();
+        if ($voteQuestions->where('votable_id', $question->id)->exists()) {
+            $voteQuestions->updateExistingPivot($question, ['vote' => $vote]);
+        }
+        else {
+            $voteQuestions->attach($question, ['vote' => $vote]);
+        }
+
+        $question->load('votes');
+        $downVotes = (int) $question->downVotes()->sum('vote');
+        $upVotes = (int) $question->upVotes()->sum('vote');
+        
+        $question->votes_count = $upVotes + $downVotes;
+        $question->save();
+    }
+
+    public function voteAnswer(Answer $answer, $vote)
+    {
+        $voteAnswers = $this->voteAnswers();
+        if ($voteAnswers->where('votable_id', $answer->id)->exists()) {
+            $voteAnswers->updateExistingPivot($answer, ['vote' => $vote]);
+        }
+        else {
+            $voteAnswers->attach($answer, ['vote' => $vote]);
+        }
+
+        $answer->load('votes');
+        $downVotes = (int) $answer->downVotes()->sum('vote');
+        $upVotes = (int) $answer->upVotes()->sum('vote');
+        
+        $answer->votes_count = $upVotes + $downVotes;
+        $answer->save();
+    }    
 }
